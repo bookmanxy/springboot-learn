6f5c9ee1956903bbc2f03fa79e154756e2d1d7ef
watermelon <994019959@qq.com>
2019-12-15 14:30:48 +0800

    加密模块-非对称加密RSA

---
+++ springboot-security/springboot-cipher/src/main/java/cn/faceland/springbootcipher/SpringbootCipherApplication.java
@@ -0,0 +1,13 @@
+package cn.faceland.springbootcipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCipherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCipherApplication.class, args);
+    }
+
+}
---
+++ springboot-security/springboot-cipher/src/test/java/cn/faceland/springbootcipher/SpringbootCipherApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.faceland.springbootcipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCipherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
